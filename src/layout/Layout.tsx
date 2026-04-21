d8b0c93b2ed1d28e3c56bf56eb97b1d38c99601e
Hemant Kirar <kirar.hemant25@gmail.com>
2026-04-22 00:04:54 +0530

    test

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -34,6 +34,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               <Link to="/catalog?cat=Women">Women</Link>
             </div>
           </div>
+          <div>random</div>
           {/*<Link to="/search">Search</Link>*/}
         </nav>
         <SearchBar />
